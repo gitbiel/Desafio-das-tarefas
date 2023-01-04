3e68469984c2e1b10ca3e8e7f2b02111c3f52206
Gabriel Mendes <gabriel.f.mend390@gmail.com>
2023-01-04 18:36:38 -0300

    contador toDo pronto

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ const btnCloseModal = document.querySelector(".closeModalContainer");
 const toDoList = document.querySelector(".backgroundColorCard");
 const inputTitleModal = document.querySelector(".inputType");
 const containerCard = document.querySelector(".containerCard");
-const main = document.getElementById('areaCard');
+const main = document.getElementById('containerToDo');
 let inputModal = document.getElementById('inputModal');
 let inputTitle = document.getElementById('titleModal');
 let inputSelect = document.getElementById('selectModal');
 let counter = 0;
-
+const toDoAccount = document.querySelector('#toDo');
+toDoAccount.innerHTML = '0'
 function openModal() {
   modal.style.display = "flex";
   toDoList.style.display = "none";
@@ -24,19 +25,20 @@ function closeModal() {
 function createCard() {
   let valorInputTilte = inputTitle.value;
   let valorInput = inputModal.value;
-  let valorInputModal = inputSelect.value;
-
-  // if ((valorInput !== "") && (valorInput !==null) && (valorInput!== undefined));
-  // if ((valorInputTilte !== "") && (valorInputTilte !==null) && (valorInputTilte!== undefined));
+  let valorInputSelect = inputSelect.value.split(', ')
+  console.log(valorInputSelect[1]);
   ++counter;
+
+
+
   let newCard = 
   `<div class="cardCreated">
-  <div class="column"></div>
+  <div class="column" style="background: ${valorInputSelect[1]};"></div>
   <div class="elements">
-    <div class="titleModal">${valorInputTilte}</div>
+    <div class="titleCard">${valorInputTilte}</div>
     <p class="type">Type:</p>
     <div class="fundo">
-      ${valorInputModal}  
+      ${valorInputSelect[0]}
     </div>
       ${valorInput}
   </div>
@@ -52,5 +54,11 @@ function createCard() {
   </div>`;
 
   main.innerHTML += newCard;
+
+  toDoAccount.innerHTML = main.childElementCount
+
+  console.log(main.childElementCount);
   closeModal()
 }
+
+console.log(main.childElementCount);
